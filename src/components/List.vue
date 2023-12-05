3054dd978b7dff657d76fdf77420f2977dfdbf6b
Spyke183 <114151896+Spyke183@users.noreply.github.com>
2023-12-05 19:44:51 +0100

    reprise

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,15 +4,21 @@
     <div v-for="contact in contacts" :key="contact.id" class="container">
       <div class="Affichage">
         <!-- Bouton pour ouvrir le menu d'actions -->
-        <div class="modif" @click="selectedContact = contact">+</div>
-        <div class="card_logo">
-          <div class="logo">
-            <img :src="contact.fields.Image" alt="Image du contact" />
-          </div>
-        </div>
-        <div class="nom">
-          {{ contact.fields.Nom }} {{ contact.fields.Prenom }}
-        </div>
+        <button1 class="modif" @click="selectedContact = contact">Modifier</button1>
+        <div class="link">
+  <a :href="contact.fields.Lien" target="_blank">
+    <div class="card_logo">
+      <div class="logo">
+        <img :src="contact.fields.Image" alt="Image du contact" />
+      </div>
+    </div>
+    <div class="nom">
+      {{ contact.fields.Nom }} 
+    </div>
+  </a>
+</div>
+
+
         <!-- Menu d'actions -->
         <div class="action-menu" v-if="selectedContact === contact">
           <button class="action-item" @click="handleUpdate(contact)">Modifier le raccourci</button>
@@ -21,15 +27,34 @@
       </div>
     </div>
   </div>
+  <form @submit.prevent="updateContact(selectedId)">
+    <!-- Modale pour la modification des contacts -->
+    <div v-if="showModal" class="modal-overlay" @click.self="closeModal">
+    <div class="modal-content">
+      <h3>Modifier le raccourci</h3>
+      <form @submit.prevent="updateContact(selectedId)">
+        <div class="form-group">
+          <label for="nom">Nom</label>
+          <input type="text" id="nom" v-model="selectedContact.fields.Nom" />
+        </div>
+        <div class="form-group">
+          <label for="url">URL</label>
+          <input type="text" id="url" v-model="selectedContact.fields.URL" />
+        </div>
+        <div class="modal-actions">
+          <button type="button" @click="closeModal">Annuler</button>
+          <button type="submit">OK</button>
+        </div>
+      </form>
+    </div>
+  </div></form>
 
   <!-- Formulaire pour modifier ou créer un contact -->
   <div v-if="edit" class="contact-form">
     <label for="nom">Nom</label>
     <input type="text" name="nom" id="nom" v-model="nom" />
-    <label for="prenom">Prénom</label>
-    <input type="text" name="prenom" id="prenom" v-model="prenom" />
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" v-model="email" />
+    <label for="lien">Lien</label>
+    <input type="lien" name="lien" id="lien" v-model="lien" />
     <button @click="updateContact(selectedId)">Modifier</button>
     <button @click="edit = false">Annuler</button>
   </div>
@@ -47,8 +72,7 @@ export default {
     return {
       contacts: [],
       nom: "",
-      prenom: "",
-      email: "",
+      lien: "",
       image: "",
       selectedId: null,
       edit: false,
@@ -59,9 +83,15 @@ export default {
   methods: {
     handleResetForm() {
       this.nom = "";
-      this.prenom = "";
-      this.email = "";
+      this.lien = "";
       this.image = "";
+    },
+    openModal(contact) {
+      this.selectedContact = contact;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
     },
     openMenu(contact) {
       this.selectedContact = contact;
@@ -106,8 +136,7 @@ export default {
         body: JSON.stringify({
           fields: {
             Nom: this.nom,
-            Prenom: this.prenom,
-            Email: this.email,
+            Lien: this.lien,
             Image: this.image,
           },
         }),
@@ -123,11 +152,11 @@ export default {
     },
     handleUpdate(contact) {
       this.nom = contact.fields.Nom;
-      this.prenom = contact.fields.Prenom;
-      this.email = contact.fields.Email;
+      this.lien = contact.fields.Lien;
       this.image = contact.fields.Image;
       this.selectedId = contact.id;
       this.edit = true;
+      this.openModal(contact);
     },
     createContact() {
       fetch(`https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`, {
@@ -139,8 +168,7 @@ export default {
         body: JSON.stringify({
           fields: {
             Nom: this.nom,
-            Prenom: this.prenom,
-            Email: this.email,
+            Lien: this.lien,
             Image: this.image,
           },
         }),
@@ -187,7 +215,65 @@ export default {
 :root {
   --icon-size: 24px;
 }
+.container{
+  padding: 1em;
+}
+a{
+  text-decoration: none;
+    color: black;
+}
+.nom{
+  margin: 8px;
+}
+.modif{
+  padding: 10px 20px;
+  margin: 7px;
+  background-color: #f1f1f1;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.modif:hover {
+  background-color: #e1e1e1; 
+}
 
+.link{
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 400px; /* ou la largeur que vous préférez */
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.modal-actions {
+  text-align: right;
+}
+
+.modal-actions  {
+  margin-left: 10px;
+}
 .favoris {
   display: flex;
   flex-direction: row;
@@ -205,6 +291,7 @@ export default {
   overflow: hidden;
   height: calc(var(--icon-size) + 16px);
   width: calc(var(--icon-size) + 16px);
+  margin: 8px 15px 0px 15px;;
 }
 
 .logo {
@@ -223,12 +310,13 @@ export default {
 
 .action-menu {
   display: flex;
-    flex-direction: column;
+  flex-direction: column;
   position: absolute;
-  background-color: #000000;
+  background-color: #d1d1d1;
   box-shadow: 0 2px 5px rgba(0,0,0,0.2);
   border-radius: 4px;
   z-index: 10;
+  padding: 10px 0px 10px 0px;
 }
 
 .action-item {
@@ -242,5 +330,17 @@ export default {
 
 .action-item:hover {
   background-color: #a3a3a3;
+}
+button1 {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: #f1f1f1;
+  /* Ajoutez d'autres styles de boutons ici */
+}
+
+button:hover {
+  background-color: #e1e1e1;
 }
 </style>
